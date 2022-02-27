52de3e036162c1f86bda1897e59bd3395256194d
Eduardo Russo <russoedu@gmail.com>
2022-02-27 07:46:15 +0000

    Improved the logo to deal with the svg instead of having two loaded svg files

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,11 @@
 import { Link } from 'react-router-dom'
 import './Logo.css'
-import logo from '../assets/img/logo-white.svg'
-import icon from '../assets/img/icon.svg'
+import { ReactComponent as LogoSvg } from '../assets/img/logo-white.svg'
+
 export function Logo () {
   return(
     <Link id='logo' to='/'>
-      <img className='logo-large' src={logo} alt="logo" />
-      <img className='logo-small' src={icon} alt="logo" />
+      <LogoSvg />
     </Link>
   )
 }
